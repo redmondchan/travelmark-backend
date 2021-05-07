099d13ab92b8e9dc85a0990ae393e5279f7b6723
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-07 15:34:23 -0400

    commented out component scan

--- src/main/java/com/redmondchan/travelmark/TravelmarkApplication.java
+++ src/main/java/com/redmondchan/travelmark/TravelmarkApplication.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.ComponentScan;
 
 import com.redmondchan.travelmark.controllers.CountryController;
 
-@SpringBootApplication
-@ComponentScan(basePackageClasses = CountryController.class)
-public class TravelmarkApplication {
 
+@SpringBootApplication
+//@ComponentScan(basePackageClasses = CountryController.class)
+public class TravelmarkApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TravelmarkApplication.class, args);
+		
 	}
+	
+	
 
 }

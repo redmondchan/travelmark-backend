fed29ef5bd2d831984e8802222fea46361cd3f81
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-06 20:36:49 -0400

    added relationship between city and article

--- src/main/java/com/redmondchan/travelmark/models/Article.java
+++ src/main/java/com/redmondchan/travelmark/models/Article.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -16,6 +18,10 @@ public class Article {
 	private int id;
 	private String title;
 	private String link;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_city_id", referencedColumnName = "id")
+	private City city;
 	
 	public int getId() {
 		return id;
--- src/main/java/com/redmondchan/travelmark/models/City.java
+++ src/main/java/com/redmondchan/travelmark/models/City.java
@@ -1,5 +1,7 @@
 package com.redmondchan.travelmark.models;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -24,6 +27,9 @@ public class City {
 	@ManyToOne
 	@JoinColumn(name = "fk_country_id", referencedColumnName = "id")
 	private Country country;
+	
+	@OneToMany(mappedBy = "city")
+	private List<Article> articles;
 	
 	public int getId() {
 		return id;

1c16d25d5eb94648657c1c004a7f5a26e135e1fb
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-07 15:36:25 -0400

    added endpoint to use api to populate database with countries

--- src/main/java/com/redmondchan/travelmark/controllers/CountryController.java
+++ src/main/java/com/redmondchan/travelmark/controllers/CountryController.java
@@ -1,8 +1,16 @@
 package com.redmondchan.travelmark.controllers;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Scanner;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,8 +39,9 @@ public class CountryController {
 	 
 	 @PostMapping("/country")
 	 ResponseEntity<Country> createCountry(@RequestBody Country country) throws URISyntaxException {
+		 System.out.println(country.toString());
 		 Country result = countryRepository.save(country);
-		 System.out.println("test");
+		 System.out.println(country.toString());
 		 return ResponseEntity.created(new URI("/country" + result.getId())).body(result);
 	 }
 	 
@@ -44,6 +53,61 @@ public class CountryController {
 	 @GetMapping("/deleteAllCountries")
 	 public ResponseEntity<Object> deleteAll() {
 		 countryRepository.deleteAll();
+		 return new ResponseEntity<>(countryRepository.findAll(), HttpStatus.OK);
+	 }
+	 
+	 //endpoint to utilize an api to populate the database with countries and cities
+	 @GetMapping("/createDb")
+	 public ResponseEntity<Object> createDb() {
+			try {
+				
+				URL url = new URL("https://countriesnow.space/api/v0.1/countries");
+				
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.connect();
+				
+				//Get response code
+				int responseCode = conn.getResponseCode();
+				
+				if(responseCode != 200) {
+					throw new RuntimeException("HttpResponseCode:" + responseCode);
+				} else {
+					String inLine = "";
+					Scanner scanner = new Scanner(url.openStream());
+					
+					//write all json data into a string 
+					while(scanner.hasNext()) {
+						inLine += scanner.nextLine();
+					}
+					
+					scanner.close();
+					
+					JSONParser parse = new JSONParser();
+					JSONObject dataObj = (JSONObject) parse.parse(inLine);
+					
+					//JSONObject obj = (JSONObject) dataObj;
+					//System.out.println(obj.get("msg"));
+					
+					JSONArray countryArr = (JSONArray) dataObj.get("data");
+					for(int i = 0; i < countryArr.size(); i++) {
+						JSONObject newObject = (JSONObject) countryArr.get(i);
+						//System.out.println(newObject.get("country"));
+						String countryName = (String) newObject.get("country");
+						//System.out.println(countryName);
+						Country country = new Country();
+						country.setName(countryName);
+						System.out.println(country.toString());
+						Country savedCountry = countryRepository.save(country);
+	 				}
+					
+				}
+				
+			} catch (IOException | ParseException e) {
+				// TODO Auto-generated catch block
+				System.out.println("in catch");
+				e.printStackTrace();
+			}
 		 return new ResponseEntity<>(countryRepository.findAll(), HttpStatus.OK);
 	 }
 }

f13388d66474e74f580314aef0ba8ac15b3585dc
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-11 20:35:20 -0400

    "created custom query to find cities by country id"

--- src/main/java/com/redmondchan/travelmark/repository/CityRepository.java
+++ src/main/java/com/redmondchan/travelmark/repository/CityRepository.java
@@ -1,9 +1,14 @@
 package com.redmondchan.travelmark.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.redmondchan.travelmark.models.City;
 
 public interface CityRepository extends JpaRepository<City, Integer> {
-
+	
+	@Query("SELECT city FROM City city WHERE city.country.id = ?1")
+	public List<City> findCitiesByCountryId(int countryId);
 }

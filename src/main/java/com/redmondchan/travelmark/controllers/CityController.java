5ac157c06d09d4cda1dc2a5e686923e678b87224
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-14 17:46:46 -0400

    added cross origin annotation

--- src/main/java/com/redmondchan/travelmark/controllers/CityController.java
+++ src/main/java/com/redmondchan/travelmark/controllers/CityController.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,7 @@ public class CityController {
 		 return new ResponseEntity<>(cityRepository.findAll(), HttpStatus.OK);
 	 }
 	 
+	 @CrossOrigin
 	 @GetMapping("/findCitiesByCountryId/{id}")
 	 public ResponseEntity<Object> findCitiesByCountryId(@PathVariable("id") int id) {
 		 cityRepository.findCitiesByCountryId(id);

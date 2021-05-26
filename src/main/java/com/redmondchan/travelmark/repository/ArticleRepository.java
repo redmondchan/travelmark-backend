0a2b86edaad50f4890d62bf9f2e21f605ad63eff
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-26 17:26:41 -0400

    created custom query to find articles by country id

--- src/main/java/com/redmondchan/travelmark/repository/ArticleRepository.java
+++ src/main/java/com/redmondchan/travelmark/repository/ArticleRepository.java
@@ -1,9 +1,16 @@
 package com.redmondchan.travelmark.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.redmondchan.travelmark.models.Article;
+import com.redmondchan.travelmark.models.City;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
+	@Query("SELECT article FROM Article article WHERE article.city.country.id = ?1")
+	public List<Article> findArticlesByCountry(int countryId);
+	
 }
